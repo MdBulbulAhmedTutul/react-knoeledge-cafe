573abb797246787c04067aaccaae2a0bb4415e37
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-09-11 09:50:38 -0700

    blog file added there

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Blogs from './components/blogs/blogs'
 import Header from './components/header/header'
-
+import Bookmarks from './components/bookmarks/bookmarks'
 function App() {
 
   return (
     <>
-    <Header></Header>
-    <Blogs></Blogs>
+      <Header></Header>
+      <div className='max-w-7xl mx-auto px-3 my-16 md:flex md: items-center'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,9 @@
-
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
+        <div className='flex items-center justify-between border-b-2 py-2 max-w-7xl mx-auto px-3'>
             <h1 className='text-4xl font-semibold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
         </div>
     );
 };
