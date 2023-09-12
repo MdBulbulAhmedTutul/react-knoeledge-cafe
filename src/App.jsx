88c048c9166740dfbf2118b747b629f0197b2a1c
MdBulbulAhmedTutul <tutulwebdevoloper@gmail.com>
2023-09-11 23:53:23 -0700

    added a new section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.css'
 import Blogs from './components/blogs/blogs'
 import Header from './components/header/header'
 import Bookmarks from './components/bookmarks/bookmarks'
+import { useState } from 'react'
+import Blog from './components/si-blog/Blog'
 function App() {
-
+  const [bookMarks, setBookMarks] = useState([]);
+  const handleAddToBookMark = blog => {
+    console.log('bookmark adding soon');
+  }
   return (
     <>
       <Header></Header>
-      <div className='max-w-7xl mx-auto px-3 my-16 md:flex md: items-center'>
-        <Blogs></Blogs>
+      <div className='max-w-7xl mx-auto px-3 my-16 md:flex '>
+        <Blogs handleAddToBookMark={handleAddToBookMark}></Blogs>
         <Bookmarks></Bookmarks>
       </div>
     </>
--- src/components/si-blog/Blog.jsx
+++ src/components/si-blog/Blog.jsx
@@ -1,9 +1,32 @@
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
+import { FaBookmark } from 'react-icons/fa';
+const Blog = ({ blog,  handleAddToBookMark }) => {
     console.log(blog);
+    const { title, cover, reading_time, author_img, author, posted_time, hashtags } = blog;
     return (
-        <div>
-            
+        <div className='my-7 border-b-2'>
+            <img className='w-full h-72' src={cover} alt="" />
+            <div className='flex justify-between items-center'>
+                <div className='flex justify-between items-center my-6'>
+                    <div>
+                        <img src={author_img} alt="" />
+                    </div>
+                    <div className='ml-6'>
+                        <p className='text-xl font-medium'>{author}</p>
+                        <p><small>{posted_time}</small></p>
+                    </div>
+                </div>
+                <div className='flex items-center'>
+                    <p>{reading_time}min read</p>
+                    <button onClick={handleAddToBookMark} className='text-red-600 ml-2 text-2xl'><FaBookmark></FaBookmark></button>
+                </div>
+            </div>
+            <h2 className='text-3xl font-semibold'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hashtags}</a></span>)
+                }
+            </p>
         </div>
     );
 };
